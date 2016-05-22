1617ac3135ccfb3e2496e69dbaef70ae021ad318
Adrian <alu0100288216@ull.edu.es>
2016-05-22 13:45:23 +0100

    Implementado de mejor manera el editor 'ace' y añadidos ficheros de ejemplos.

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,18 +1,16 @@
-YUI().use(
-  'aui-ace-editor',
-  function(ed) {
+var editor = ace.edit("myEditor");
+//editor.setTheme("src-min/theme-github.js");
+editor.setTheme("ace/theme/monokai");
+//editor.session.setValue($("#my_html_hidden").text());
 
-  }
-);
+function loadFile(fileName) {
+  $.ajax({
+    url : fileName,
+    dataType: "text",
+    success : function (data) {
+      editor.session.setValue(data);
 
-YUI().use(
-  'aui-ace-editor',
-  function(ed) {
-    new ed.AceEditor(
-      {
-        boundingBox: '#myEditor',
-        //mode: 'javascript'
+      if (window.localStorage) {localStorage.original = data;}
       }
-    ).render();
-  }
-);
+  });
+};
